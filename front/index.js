59bb6d9ba395774f4753b417fc3657ff0a4862e6
Raül Pérez <repejota@gmail.com>
2016-03-29 01:05:47 +0200

    Added sessions

--- front/index.js
+++ front/index.js
@@ -1,5 +1,16 @@
 var express = require('express');
+var session = require('express-session');
+var redisStore  = require('connect-redis')(session);
+var redis = require("redis");
+var client = redis.createClient();
 var app = express();
+
+app.use(session({
+    secret: 'ssshhhhh',
+    store: new redisStore({ host: 'redis', port: 6379, client: client,ttl :  260}),
+    saveUninitialized: false,
+    resave: false
+}));
 
 app.get('/', function (req, res) {
   res.send('Hello World! This is the front app microservice.');
